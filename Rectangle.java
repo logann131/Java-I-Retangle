2d0d9ab73ea0de66cefa781ae3ed8d79573c2792
lgad31vn <66233296+lgad31vn@users.noreply.github.com>
2021-04-11 02:11:39 -0500

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,34 @@
+public class Rectangle{
+   private double width; 
+   private double height;
+   public Rectangle() {
+   width = 1;
+   height = 1;
+   }
+   public Rectangle(double w, double h){
+   width = w;
+   height = h;
+   }
+   public double getWidth() {
+   return width;
+   }
+   public double getHeight() {
+   return height;
+   }
+   public double getArea() {  
+   return width * height;
+   }
+   public double getPerimeter() {
+   return 2 *(width + height);
+   }   
+   public String toString(){
+   return ("Rectangle width = "+ width + ",height = "+ height);
+   }
+   public boolean equals(Rectangle rect){
+   Boolean sameHeight, sameWidth;
+   sameHeight = (getHeight() ==  rect.getHeight() );
+   sameWidth  = (getWidth() ==  rect.getWidth());
+   return (sameHeight && sameWidth);
+   }
+   
+   }
